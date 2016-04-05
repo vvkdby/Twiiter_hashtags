2a0277b662f17f39fe3d52bad2e9f1428cc22327
vvkdby <vvkdby@users.noreply.github.com>
2016-04-04 20:37:51 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-
-
+# If this does not work, you can also look at the readme for running instructions
 python ./src/average_degree.py 
-./tweet_input/tweets.txt 
+# Enter the input file
+./tweet_input/tweets.txt
+# Enter the output file
 ./tweet_output/output.txt
 
 
